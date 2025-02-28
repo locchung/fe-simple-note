ae81370f3df76c79c9529815337fefd8d1c368a1
Loc Chung <huuloc939@gmail.com>
2025-02-28 16:51:00 +0700

    Update redirect destination from /auth/login to /auth/signup

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/auth/login',
+        destination: '/auth/signup',
         permanent: true,
       },
     ];
