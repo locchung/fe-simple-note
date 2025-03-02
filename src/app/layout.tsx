0a15c10079170f8b98c4e1e1b2b6d952139b1a3b
Loc Chung <huuloc939@gmail.com>
2025-03-02 15:05:21 +0700

    Temporarily add Content Security Policy meta tag to bypass mixed content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <AuthProvider>
         <ThemeProvider>
           <body>{children}</body>
